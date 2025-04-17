dadfb924ce5267b6138ef57fb22f9b7a8578f247
michelelongo97 <m.longo7x@gmail.com>
2025-04-17 12:42:13 +0200

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const products = [
     { name: "Mela", price: 0.5 },
@@ -6,6 +8,17 @@ function App() {
     { name: "Pasta", price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  const addToCart = (product) => {
+    const isProductInCart = addedProducts.some(
+      (addedProduct) => addedProduct.name === product.name
+    );
+    if (isProductInCart) {
+      return;
+    }
+    setAddedProducts((curr) => [...curr, { ...product, quantity: 1 }]);
+  };
   return (
     <>
       <h1>Prodotti</h1>
@@ -16,10 +29,28 @@ function App() {
               <p>
                 {product.name} €{product.price.toFixed(2)}
               </p>
+              <button onClick={() => addToCart(product)}>Add to Cart</button>
             </li>
           );
         })}
       </ul>
+      {addedProducts.length > 0 && (
+        <>
+          <h2>Cart</h2>
+          <ul>
+            {addedProducts.map((product, index) => {
+              return (
+                <li key={index}>
+                  <p>
+                    {product.name} €{product.price.toFixed(2)} Qt.
+                    {product.quantity}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </>
+      )}
     </>
   );
 }
