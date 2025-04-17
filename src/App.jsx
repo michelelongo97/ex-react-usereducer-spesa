f9292b72b85765aeebc8eba1a72d04ca0e935c02
michelelongo97 <m.longo7x@gmail.com>
2025-04-17 13:23:43 +0200

    feat: add milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,36 @@ function App() {
 
   const [addedProducts, setAddedProducts] = useState([]);
 
+  const updateProductQuantity = (name, quantity) => {
+    setAddedProducts((curr) =>
+      curr.map((product) => {
+        if (product.name === name) {
+          return { ...product, quantity };
+        }
+        return product;
+      })
+    );
+  };
+
   const addToCart = (product) => {
-    const isProductInCart = addedProducts.some(
+    const productInCart = addedProducts.find(
       (addedProduct) => addedProduct.name === product.name
     );
-    if (isProductInCart) {
+    if (productInCart) {
+      updateProductQuantity(productInCart.name, productInCart.quantity + 1);
       return;
     }
     setAddedProducts((curr) => [...curr, { ...product, quantity: 1 }]);
   };
+
+  const removeFromCart = (product) => {
+    setAddedProducts((curr) => curr.filter((p) => p.name !== product.name));
+  };
+
+  const totalPrice = addedProducts.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+  }, 0);
+
   return (
     <>
       <h1>Prodotti</h1>
@@ -45,10 +66,15 @@ function App() {
                     {product.name} €{product.price.toFixed(2)} Qt.
                     {product.quantity}
                   </p>
+                  <button onClick={() => removeFromCart(product)}>
+                    Remove from Cart
+                  </button>
                 </li>
               );
             })}
           </ul>
+          <hr />
+          <h4>Total price to pay: €{totalPrice.toFixed(2)}</h4>
         </>
       )}
     </>
