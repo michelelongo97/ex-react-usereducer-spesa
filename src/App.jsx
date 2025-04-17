50a684660a1c057d56ac16532c709d699b0dde1e
michelelongo97 <m.longo7x@gmail.com>
2025-04-17 12:12:28 +0200

    feat: add milestone 1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  const products = [
+    { name: "Mela", price: 0.5 },
+    { name: "Pane", price: 1.2 },
+    { name: "Latte", price: 1.0 },
+    { name: "Pasta", price: 0.7 },
+  ];
+
+  return (
+    <>
+      <h1>Prodotti</h1>
+      <ul>
+        {products.map((product, index) => {
+          return (
+            <li key={index}>
+              <p>
+                {product.name} €{product.price.toFixed(2)}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+export default App;
